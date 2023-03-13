bee95690a37fbb3f9b4e113a42b0d23c5b0e2f8a
윤석규 <devSxxkgyu@gmail.com>
2023-03-13 10:50:00 +0900

    Init : Project setting

---
+++ app/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/dauth_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.hs.dgsw.smartschool.dodamdodam.dauth_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
